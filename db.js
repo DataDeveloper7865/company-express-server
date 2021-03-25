2975f3f24eba9debab136841d83501c8712b5f83
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-03-25 16:12:29 -0700

    added some tests not working

--- db.js
+++ db.js
@@ -3,7 +3,7 @@
 const { Client } = require("pg");
 
 const DB_URI = process.env.NODE_ENV === "test"
-    ? "postgresql://biztime_test"
+    ? "postgresql:///biztime_test"
     : "postgresql:///biztime";
 
 let db = new Client({
